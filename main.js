f74842e2abddbb0521399a75c1a9c456f84c83b4
Gerald2309 <68652749+Gerald2309@users.noreply.github.com>
2022-02-10 17:11:01 -0500

    Creación de barras laterales

--- main.js
+++ main.js
@@ -11,8 +11,34 @@
         get elements()
         {
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
+
+        }
+    }
+
+})();
+
+(function()
+{
+    self.Bar = function(x,y,width,height,board) 
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        this.board.bars.push(this); //Agrega este elemento en el arreglo bars
+        this.kind = "rectangle"; // para decirle al canvas que este elemento es un rectángulo
+        console.log("Hello ");
+    }
+
+    self.Bar.prototype = {
+        down: function(){
+
+        },
+        up: function(){
 
         }
     }
@@ -30,6 +56,32 @@
 
     }
 
+    self.BoardView.prototype = {
+        draw: function()
+        {
+            for (var i = this.board.elements.length - 1; i >= 0; i--){
+                var el = this.board.elements[i];
+
+                draw(this.ctx,el)
+            };
+        }
+    }
+
+    function draw(ctx, element){
+
+        if (element !== null && element.hasOwnProperty("kind"))
+        {
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height)
+                    break;
+            }
+    
+
+        } 
+        
+    }
+
 })();
 
 window.addEventListener("load", main);
@@ -37,7 +89,12 @@ window.addEventListener("load", main);
 function main(){
 
     var board = new Board(800,400);
-    var canvas = document.getElementById('canvas', ba);
-    var board_view = new BoardView();
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas, board);
+    var bar = new Bar(20,100,40,100, board); 
+    var bar = new Bar(740,100,40,100, board); 
+
+    console.log(board);
+    board_view.draw();
 
 }
