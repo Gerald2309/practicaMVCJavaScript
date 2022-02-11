b79e8ea9ced8f40d9735a9b33758fb66acd40c11
Gerald2309 <68652749+Gerald2309@users.noreply.github.com>
2022-02-11 18:01:17 -0500

    Movimiento de la pelota y pausa del juego

--- main.js
+++ main.js
@@ -6,11 +6,12 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
     self.Board.prototype = {
         get elements()
         {
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;});
             elements.push(this.ball);
             return elements;
         }
@@ -27,9 +28,20 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
 
         board.ball = this;
         this.kind = "circle"
+
+        
+    }
+    self.Ball.prototype = {
+        move: function()
+        {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+
+        }
     }
 
 })();
@@ -90,8 +102,14 @@
         },
         play: function()
         {
-            this.clean();
-            this.draw();
+            if(this.board.playing)
+            {
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+
+            }
+            
 
         }
     }
@@ -125,28 +143,36 @@ var ball = new Ball(350,100,10,board);
 
 document.addEventListener("keydown", function(ev)
 {
-    ev.preventDefault();
     if(ev.keyCode == 38)
     {
+        ev.preventDefault();
         bar1.up();
     }
     else if (ev.keyCode == 40)
     {
+        ev.preventDefault();
         bar1.down();
     }
     else if (ev.keyCode == 83)
-    {
+    {//s
+        ev.preventDefault();
         bar2.down();
     }
     else if (ev.keyCode == 87)
-    {
+    {//w
+        ev.preventDefault();
         bar2.up();
     }
-    console.log(" "+bar2);
+    else if(ev.keyCode === 32){
+        ev.preventDefault();
+        board.playing = !board.playing;
+
+    }
 });
 
 window.requestAnimationFrame(controller);
 
+board_view.draw();
 function controller(){
     board_view.play();
     window.requestAnimationFrame(controller);
